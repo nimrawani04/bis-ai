ea2512dae42b62156c325735f8b9ea164d432de3
Nimra Wani <nimrawani04@gmail.com>
2026-03-08 13:15:54 +0000

    Preceding changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Index from "./pages/Index";
+import ProductPassport from "./pages/ProductPassport";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
